494ad66b8f5c180912d274c18ced2eb8885eb848
Adrian Hamelink <adrian.hamelink@gmail.com>
2019-03-18 16:57:12 +0100

    Initial Commit

---
+++ VFD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VFD
+//
+//  Created by Adrian Hamelink on 18.03.19.
+//  Copyright © 2019 LACAL. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
