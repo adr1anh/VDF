be5bbff257734ab7cdc3a041c1d9ef3bc2721283
AlessioAttanasio <alessio.attanasio@epfl.ch>
2019-04-29 15:17:04 +0200

    Rien
    Rien

--- VDF/main.cpp
+++ VDF/main.cpp
@@ -13,6 +13,9 @@
 #include <openssl/rsa.h>
 #include <openssl/sha.h>
 #include <vector>
+#include <cmath>
+
+using namespace std;
 
 void generate_rsa_prime(mpz_class &p,
                         mpz_class &q,
@@ -20,7 +23,6 @@ void generate_rsa_prime(mpz_class &p,
 
 void hash_prime();
 
-//gf
 //https://stackoverflow.com/a/2262447
 bool simpleSHA256(void* input, unsigned long length, unsigned char* md)
 {
@@ -60,59 +62,97 @@ bool verify(mpz_class const& pk,
             mpz_class const& proof,
             unsigned short iterations);
 
-mpz_class* proof(mpz_class const& id, mpz_class const& l, mpz_class const& t, mpz_class const& kappa, mpz_class const& gamma, vector<mpz_class> const& base)
+int proof(int const& id, int const& l, int const& t, int const& kappa, int const& gamma, vector<unsigned long long int> const& base)
 {
-    mpz_class kappa1 = floor(*kappa/2);
-    mpz_class kappa0 = *kappa - kappa1;
-    x = unique_ptr<mpz_class>(new mpz_class(*id));
+    int kappa1 = floor((double)kappa/(double)2);
+    int kappa0 = kappa - kappa1;
+    int x = id;
     
-    for (mpz_class j = gamma - 1, j >= 0, j=j-1)
+    for (int j = gamma -1; j >= 0; j = j-1)
     {
-        *x = x^(2^*kappa);
-        y = new array<mpz_class, 2^(*kappa)>;
+        x = pow(x,(1 << kappa));
+        size_t length_of_vector = (1<<kappa);
+        vector<unsigned long long int> y(length_of_vector);
         
-        for(mpz_class b = 0, b < 2^(*kappa), b++)
-        {
-            *y[b] = id;
-        }
+        for(int b = 0; b < (1<<kappa); b++)
+            y[b] = id;
         
-        for (mpz_class i = 0, i < ceil(*t/(*kappa * *gamma))), i++)
+        int upper_bound = ceil((double)t/(kappa * gamma));
+        for (int i = 0; i < upper_bound; i++)
         {
-            b = floor((2^*kappa * (2^(*t - *kappa * (i * *gamma + j)) % *l)/ *l);
-            *y[b] = *y[b] * *base[i];
+            unsigned long long int help = (1 << (t - kappa * (i * gamma + j + 1)));
+            unsigned long long int a, c;
+            a=(1<<kappa);
+            c=(help % l);
+            unsigned long long int b = (double)(a * c) / (double)l;
+            y[b] = y[b] * base[i];
+            //cout << y[0] << endl;
+            //cout << y[1] << endl;
         }
-                      
-        for(mpz_class b1 = 0, b1 < 2^kappa1, b1++)
+        unsigned long long int z;
+        for(int b1 = 0; b1 < (1<<kappa1); b1++)
         {
             z = id;
-            for(mpz_class b0 = 0, b0 < 2^(*kappa), b0++)
+            for(int b0 = 0; b0 < (1<<kappa0); b0++)
             {
-                z = z * *y[b1 * 2^kappa0 + b0];
+                int idx = b1 * (1<<kappa0) + b0;
+                z = z * y[idx];
             }
-            x = x * z^(b1 * 2^kappa0);
+            
+            x = x * pow(z,b1 * (1<<kappa0));
         }
                       
-        for(mpz_class b0 = 0, b0 < 2^kappa0, b0++)
+        for(int b0 = 0; b0 < (1<<kappa0); b0++)
         {
             z = id;
-            for(mpz_class b1 = 0, b1 < 2^kappa1, b1++)
+            for(int b1 = 0; b1 < (1<<kappa1); b1++)
             {
-                z = z * *y[b1 * 2^kappa0 + b0];
+                z = z * y[b1 * (1<<kappa0) + b0];
             }
-            x = x * z^b0;
+            x = x * pow(z,b0);
         }
     }
-    delete y;
     return x;
 }
 
-static unsigned short int k = 128;
+//static unsigned short int k = 128;
 
 int main(int argc, const char * argv[]) {
-    mpz_class a,b;
-    a= 5;
-    b=3;
-    hash(a, b);
-    std::cout << "Hello, Alessio!\n";
+    int g = 2;
+    int l = 9;
+    int t = 5;
+    int kappa = 1;
+    int gamma = 1;
+    // We fail... often
+    
+    //VDF function
+    unsigned long long int y = g;
+    for (int i = 0; i < t; i++)
+        y = y * y;
+    
+    //Computation of the basis elements
+    vector<unsigned long long int> test;
+    for (int i = 0; i < 5; i++)
+        test.push_back(pow(g,pow(2,i*kappa*gamma)));
+
+    //Computation of the proof via our function
+    unsigned long long int pi = proof(1,l,t,kappa,gamma,test);
+    
+    //Computation of the proof directly
+    unsigned long long int pi_original = pow(g,floor(pow(2,t)/l));
+    
+    //Output of the results
+    cout << "The VDF outputs " << y << endl;
+    cout << "Computed proof: " << pi << endl;
+    cout << "Original proof: " << pi_original << endl;
+    if (pi == pi_original)
+        cout << "SUCCESS" << endl;
+    else
+        cout << "FAILURE" << endl;
+    
+   //Output of our base elements
+    for ( size_t i = 0; i < test.size(); i++ )
+        cout << "Element " << i << " of our base array is: " << test[i] << endl;
+    
     return 0;
 }
