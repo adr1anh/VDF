0a054320c327d58418cb6724f49a7e6da6d7f090
Adrian Hamelink <adrian.hamelink@gmail.com>
2019-06-05 10:47:46 +0200

    Updates

--- VDF/main.c
+++ VDF/main.c
@@ -61,12 +61,12 @@ int main(int argc, const char * argv[]) {
         gmp_randstate_t state;
         gmp_randinit_default(state);
         gmp_randseed_ui(state, seed); //Use a seed to make output reproducible
-        
+
         mpz_t pk;
         mpz_init(pk);
-        
+
         generate_prime(pk, state, rsa_key_length);
-        for (int t_exp = 10; t_exp < 26; ++t_exp) {
+        for (int t_exp = 10; t_exp < 32; ++t_exp) {
             struct timespec start, finish;
             double t_total, t_verify,t_verify_prime;
 
@@ -132,8 +132,95 @@ int main(int argc, const char * argv[]) {
         mpz_clear(pk);
     }
     fclose(fp);
-    
+
     fp = fopen("/Users/Adrian/Desktop/graph_overhead.csv", "w");
+    fprintf(fp,"modulus,n,input,output,proof,prime,t,gamma,k,bound,segments,overhead,t_squaring,t_proof,t_total,t_verify,t_verify_prime\n");
+    for (int i = 8; i < 13; ++i) {
+        rsa_key_length = 1 << i;
+        gmp_randstate_t state;
+        gmp_randinit_default(state);
+        gmp_randseed_ui(state, seed); //Use a seed to make output reproducible
+
+        mpz_t pk;
+        mpz_init(pk);
+        generate_prime(pk, state, rsa_key_length);
+        for (int segment = 0; segment < 4; ++segment) {
+            struct timespec start, finish;
+            double t_total, t_verify,t_verify_prime;
+
+            t = 1 << 22;
+            ProofData* output = malloc(sizeof(ProofData)*(1+segment));
+
+            GroupElement input = group_init_set_ui(&pk, 14091996);
+
+            clock_gettime(CLOCK_MONOTONIC, &start);
+            eval(output, input, t, 20.0, segment+1);
+            clock_gettime(CLOCK_MONOTONIC, &finish);
+            t_total = (finish.tv_sec - start.tv_sec);
+            t_total += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+
+
+            clock_gettime(CLOCK_MONOTONIC, &start);
+            for (int j =0; j<segment+1;j++){
+                assert(verify(output[j]) == 0);
+            }
+            clock_gettime(CLOCK_MONOTONIC, &finish);
+            t_verify = (finish.tv_sec - start.tv_sec);
+            t_verify += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+
+
+
+            clock_gettime(CLOCK_MONOTONIC, &start);
+            for (int j =0; j<segment+1;j++){
+                assert(verify_prime(output[j])==0);
+            }
+            clock_gettime(CLOCK_MONOTONIC, &finish);
+            t_verify_prime = (finish.tv_sec - start.tv_sec);
+            t_verify_prime += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+            
+
+            printf("%u,%llu,%.7f,%.7f,%.7f,%.7f,%.7f\n",
+                   rsa_key_length,
+                   output[segment].t,
+                   output[segment].time_squaring,
+                   output[segment].time_proof,
+                   t_total,
+                   t_verify,
+                   t_verify_prime);
+            fprintf(fp,"%u,%s,%s,%s,%s,%s,%llu,%llu,%u,%llu,%u,%.7f,%.7f,%.7f,%.7f,%.7f,%.7f\n",
+                    rsa_key_length,
+                    mpz_get_str(NULL, 10, pk),
+                    group_get_string(output[segment].input),
+                    group_get_string(output[segment].output),
+                    group_get_string(output[segment].proof),
+                    mpz_get_str(NULL, 10, output[segment].prime),
+                    output[segment].t,
+                    output[segment].gamma,
+                    output[segment].k,
+                    output[segment].bound,
+                    segment,
+                    overhead,
+                    output[segment].time_squaring,
+                    output[segment].time_proof,
+                    t_total,
+                    t_verify,
+                    t_verify_prime); // printing to file
+            vdf_clear_outputs(output, 1);
+            free(output);
+
+//            if (t_total > 600.0) {
+//                break;
+//            }
+        }
+        mpz_clear(pk);
+    }
+    fclose(fp);
+    
+    
+    fp = fopen("/Users/Adrian/Desktop/graph_overhead_opt.csv", "w");
     fprintf(fp,"modulus,n,input,output,proof,prime,t,gamma,k,bound,segments,overhead,t_squaring,t_proof,t_total,t_verify,t_verify_prime\n");
     for (int i = 8; i < 16; ++i) {
         rsa_key_length = 1 << i;
@@ -144,22 +231,26 @@ int main(int argc, const char * argv[]) {
         mpz_t pk;
         mpz_init(pk);
         generate_prime(pk, state, rsa_key_length);
-//        for (int t_exp = 10; t_exp < 26; ++t_exp) {
-        for (int segment = 0; segment < 4; ++segment) {
+        double overhead = 42.0;
+        for (int segment = 0; segment < 6; ++segment) {
             struct timespec start, finish;
             double t_total, t_verify,t_verify_prime;
             
-            t = 1 << 22;
+            t = 1 << 25;
             ProofData* output = malloc(sizeof(ProofData)*(1+segment));
             
             GroupElement input = group_init_set_ui(&pk, 14091996);
             
             clock_gettime(CLOCK_MONOTONIC, &start);
-            eval(output, input, t, 20.0, segment+1);
+            eval(output, input, t, overhead, segment+1);
             clock_gettime(CLOCK_MONOTONIC, &finish);
             t_total = (finish.tv_sec - start.tv_sec);
             t_total += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
             
+            
+            if (segment == 0) {
+                overhead = t_total / output[0].time_squaring;
+            }
             
             
             clock_gettime(CLOCK_MONOTONIC, &start);
@@ -250,6 +341,6 @@ int main(int argc, const char * argv[]) {
 //    free(outputs);
 //    outputs = NULL;
     
-    mpz_clears(two, pk, NULL);
+    mpz_clears(two, NULL);
     return 0;
 }
--- plot.py
+++ plot.py
@@ -8,51 +8,82 @@ import matplotlib
 import time
 
 
-df=pd.read_csv('graph_modulus.csv', sep=',')
-df.values
+# modulus_data=pd.read_csv('graph_modulus.csv', sep=',')
+
+# fig, ax1 = plt.subplots()
+
+# colors = {
+#     256: 'b',
+#     512: 'g',
+#     1024: 'r',
+#     2048: 'c',
+#     4096: 'm',
+#     8192: 'y',
+#     16384: 'k',
+#     32768: 'b'
+# }
+
+# l7 = []
+# for key, grp in modulus_data.groupby(['modulus']):
+#     if key == 256:
+#         l1,=plt.plot(grp['t'].apply(np.math.log2), grp['t_total'], label='Total time', color=colors[key])         
+#         l2,=plt.plot(grp['t'].apply(np.math.log2), grp['t_squaring'], label='Squaring time', linestyle='dashed', color=colors[key])
+#         l3,=plt.plot(grp['t'].apply(np.math.log2), grp['t_proof'], label='Proof time', linestyle='dotted', color=colors[key])
+#     else:
+#         l4,=plt.plot(grp['t'].apply(np.math.log2), grp['t_total'], label = "RSA{0:02d}".format(key), color=colors[key])         
+#         l5,=plt.plot(grp['t'].apply(np.math.log2), grp['t_proof'], color=colors[key], label='_nolegend_', linestyle='dotted')
+#         l6,=plt.plot(grp['t'].apply(np.math.log2), grp['t_squaring'], color=colors[key], label='_nolegend_', linestyle='dashed')
+#         l7.extend([l4])
+
+# first_legend = plt.legend(handles=[l1,l2,l3], loc=1)
+# plt.gca().add_artist(first_legend)
+# plt.legend(handles=l7, loc=2)
+# ax1 = plt.gca()
+# formatter_time = matplotlib.ticker.FuncFormatter(lambda s, x: time.strftime('%M:%S', time.gmtime(s)))
+# ax1.yaxis.set_major_formatter(formatter_time)
+# ax1.set_xlabel(r'$\log_2{t}$')
+# ax1.set_ylabel('Running time (minutes:seconds)')
+
+# plt.savefig('modulus.pdf')
+# plt.show()
+
+# #%%
+
+# overhead_data=pd.read_csv('/Users/Adrian/Desktop/graph_overhead.csv', sep=',')
+# overhead_data.values
+
+
+
+# fig, ax1 = plt.subplots()
+
+# colors = {
+#     0: 'b',
+#     1: 'g',
+#     2: 'r',
+#     3: 'c',
+#     4: 'm',
+# }
+
+# for key, grp in overhead_data.groupby(['segments']):
+#     if key == 0:      
+#         plt.plot(grp['modulus'].apply(np.math.log2), grp['t_squaring'], label='Time squaring', linestyle='dashed', color=colors[4])
+#     plt.plot(grp['modulus'].apply(np.math.log2), grp['t_total'], label="Time w/ {0:0d} segment(s)".format(key+1), color=colors[key])   
+
+
+# formatter_time = matplotlib.ticker.FuncFormatter(lambda s, x: time.strftime('%M:%S', time.gmtime(s)))
+# ax1.yaxis.set_major_formatter(formatter_time)
+# ax1.set_xlabel('RSA Key Size')
+# ax1.set_ylabel('Running time (minutes:seconds)')
+# plt.xticks(np.arange(8,16), 1 << np.arange(8,16))
+# plt.legend(loc='best')
+# plt.savefig('overhead.pdf')
+# plt.show()
+
 
 #%%
-fig, ax1 = plt.subplots()
 
-colors = {
-    256: 'b',
-    512: 'g',
-    1024: 'r',
-    2048: 'c',
-    4096: 'm',
-    8192: 'y',
-    16384: 'k',
-    32768: 'b'
-}
-
-l7 = []
-for key, grp in df.groupby(['modulus']):
-    if key == 256:
-        l1,=plt.plot(grp['t'].apply(np.math.log2), grp['t_total'], label='Total time', color=colors[key])         
-        l2,=plt.plot(grp['t'].apply(np.math.log2), grp['t_squaring'], label='Squaring time', linestyle='dashed', color=colors[key])
-        l3,=plt.plot(grp['t'].apply(np.math.log2), grp['t_proof'], label='Proof time', linestyle='dotted', color=colors[key])
-    else:
-        l4,=plt.plot(grp['t'].apply(np.math.log2), grp['t_total'], label = "RSA{0:02d}".format(key), color=colors[key])         
-        l5,=plt.plot(grp['t'].apply(np.math.log2), grp['t_proof'], color=colors[key], label='_nolegend_', linestyle='dotted')
-        l6,=plt.plot(grp['t'].apply(np.math.log2), grp['t_squaring'], color=colors[key], label='_nolegend_', linestyle='dashed')
-        l7.extend([l4])
-
-first_legend = plt.legend(handles=[l1,l2,l3], loc=1)
-plt.gca().add_artist(first_legend)
-plt.legend(handles=l7, loc=2)
-ax1 = plt.gca()
-formatter_time = matplotlib.ticker.FuncFormatter(lambda s, x: time.strftime('%M:%S', time.gmtime(s)))
-ax1.yaxis.set_major_formatter(formatter_time)
-ax1.set_xlabel(r'$\log_2{t}$')
-ax1.set_ylabel('Running time (minutes:seconds)')
-
-plt.savefig('modulus.pdf')
-plt.show()
-
-#%%
-
-df2=pd.read_csv('graph_overhead.csv', sep=',')
-df2.values
+overheadopt_data=pd.read_csv('/Users/Adrian/Desktop/graph_overhead_opt.csv', sep=',')
+overheadopt_data.values
 
 
 
@@ -64,12 +95,13 @@ colors = {
     2: 'r',
     3: 'c',
     4: 'm',
+    5: 'y'
 }
 
-for key, grp in df2.groupby(['segments']):
+for key, grp in overheadopt_data.groupby(['segments']):
     if key == 0:      
-        plt.plot(grp['modulus'].apply(np.math.log2), grp['t_squaring'], label='Time squaring', linestyle='dashed', color=colors[4])
-    plt.plot(grp['modulus'].apply(np.math.log2), grp['t_total'], label="Time w/ {0:0d} segment(s)".format(key+1), color=colors[key])   
+        ax1.plot(grp['modulus'].apply(np.math.log2), grp['t_squaring'], label='Time squaring', linestyle='dashed', color=colors[4])
+    ax1.plot(grp['modulus'].apply(np.math.log2), grp['t_total'], label="Time w/ {} segment(s)".format(key+1), color=colors[key])   
 
 
 formatter_time = matplotlib.ticker.FuncFormatter(lambda s, x: time.strftime('%M:%S', time.gmtime(s)))
@@ -78,9 +110,19 @@ ax1.set_xlabel('RSA Key Size')
 ax1.set_ylabel('Running time (minutes:seconds)')
 plt.xticks(np.arange(8,16), 1 << np.arange(8,16))
 plt.legend(loc='best')
+
+ax2 = ax1.twinx() 
+ax2.set_ylabel(r'$\omega$', color='y')  # we already handled the x-label with ax1
+for key, grp in overheadopt_data.groupby(['segments']):
+    if key == 1:      
+        ax2.plot(grp['modulus'].apply(np.math.log2), grp['overhead'], label='Time squaring', color=colors[5])
+
+ax2.tick_params(axis='y', labelcolor='y')
+
+
+
 plt.savefig('overhead.pdf')
 plt.show()
-
 
 
 #%%
