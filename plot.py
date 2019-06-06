a9512184b90b04304483c08f347b9664e4ad0a22
Adrian Hamelink <adrian.hamelink@gmail.com>
2019-06-06 16:49:55 +0200

    update graphs

--- plot.py
+++ plot.py
@@ -8,82 +8,67 @@ import matplotlib
 import time
 
 
-# modulus_data=pd.read_csv('graph_modulus.csv', sep=',')
+modulus_data=pd.read_csv('graph_modulus.csv', sep=',')
 
-# fig, ax1 = plt.subplots()
+plt.close('all')
+fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 
-# colors = {
-#     256: 'b',
-#     512: 'g',
-#     1024: 'r',
-#     2048: 'c',
-#     4096: 'm',
-#     8192: 'y',
-#     16384: 'k',
-#     32768: 'b'
-# }
+colors = {
+    256: 'b',
+    512: 'g',
+    1024: 'r',
+    2048: 'c',
+    4096: 'm',
+    8192: 'y',
+    16384: 'k',
+    32768: 'b'
+}
 
-# l7 = []
-# for key, grp in modulus_data.groupby(['modulus']):
-#     if key == 256:
-#         l1,=plt.plot(grp['t'].apply(np.math.log2), grp['t_total'], label='Total time', color=colors[key])         
-#         l2,=plt.plot(grp['t'].apply(np.math.log2), grp['t_squaring'], label='Squaring time', linestyle='dashed', color=colors[key])
-#         l3,=plt.plot(grp['t'].apply(np.math.log2), grp['t_proof'], label='Proof time', linestyle='dotted', color=colors[key])
-#     else:
-#         l4,=plt.plot(grp['t'].apply(np.math.log2), grp['t_total'], label = "RSA{0:02d}".format(key), color=colors[key])         
-#         l5,=plt.plot(grp['t'].apply(np.math.log2), grp['t_proof'], color=colors[key], label='_nolegend_', linestyle='dotted')
-#         l6,=plt.plot(grp['t'].apply(np.math.log2), grp['t_squaring'], color=colors[key], label='_nolegend_', linestyle='dashed')
-#         l7.extend([l4])
+all_lines = []
+for key, grp in modulus_data[modulus_data['t'] >(2**21)].groupby(['modulus']):
+    if int(np.math.log2(key)) >= 12:
+        continue
+    l,=ax1.plot(grp['t'].apply(np.math.log2), grp['t_total'], label = "RSA{0:02d}".format(key), color=colors[key])         
+    ax1.plot(grp['t'].apply(np.math.log2), grp['t_proof'], color=colors[key], label='_nolegend_', linestyle='dotted')
+    ax1.plot(grp['t'].apply(np.math.log2), grp['t_squaring'], color=colors[key], label='_nolegend_', linestyle='dashed')
+    all_lines.extend([l])
 
-# first_legend = plt.legend(handles=[l1,l2,l3], loc=1)
-# plt.gca().add_artist(first_legend)
-# plt.legend(handles=l7, loc=2)
-# ax1 = plt.gca()
-# formatter_time = matplotlib.ticker.FuncFormatter(lambda s, x: time.strftime('%M:%S', time.gmtime(s)))
-# ax1.yaxis.set_major_formatter(formatter_time)
-# ax1.set_xlabel(r'$\log_2{t}$')
-# ax1.set_ylabel('Running time (minutes:seconds)')
-
-# plt.savefig('modulus.pdf')
-# plt.show()
-
-# #%%
-
-# overhead_data=pd.read_csv('/Users/Adrian/Desktop/graph_overhead.csv', sep=',')
-# overhead_data.values
+l1,=ax1.plot([], [], label='Total time', color='k')         
+l2,=ax1.plot([], [], label='Squaring time', linestyle='dashed', color='k')     
+l3,=ax1.plot([], [], label='Proof time', linestyle='dotted', color='k')   
+all_lines.extend([l1,l2,l3])  
+ax1.legend(handles=all_lines, loc=2)
 
 
+all_lines = []
 
-# fig, ax1 = plt.subplots()
+for key, grp in modulus_data[modulus_data['t'] >(2**16)].groupby(['modulus']):
+    if int(np.math.log2(key)) < 12:
+        continue
+    l, = ax2.plot(grp['t'].apply(np.math.log2), grp['t_total'], label = "RSA{0:02d}".format(key), color=colors[key])         
+    ax2.plot(grp['t'].apply(np.math.log2), grp['t_proof'], color=colors[key], label='_nolegend_', linestyle='dotted')
+    ax2.plot(grp['t'].apply(np.math.log2), grp['t_squaring'], color=colors[key], label='_nolegend_', linestyle='dashed')
+    all_lines.extend([l])
+ax2.legend(handles=all_lines, loc=2)
 
-# colors = {
-#     0: 'b',
-#     1: 'g',
-#     2: 'r',
-#     3: 'c',
-#     4: 'm',
-# }
+formatter_time = matplotlib.ticker.FuncFormatter(lambda s, x: time.strftime('%M:%S', time.gmtime(s)))
+ax1.yaxis.set_major_formatter(formatter_time)
+ax2.yaxis.set_major_formatter(formatter_time)
+ax1.set_xlabel(r'$\log_2{t}$')
+ax2.set_xlabel(r'$\log_2{t}$')
+ax1.set_ylabel('Running time (minutes:seconds)')
+plt.tight_layout()
 
-# for key, grp in overhead_data.groupby(['segments']):
-#     if key == 0:      
-#         plt.plot(grp['modulus'].apply(np.math.log2), grp['t_squaring'], label='Time squaring', linestyle='dashed', color=colors[4])
-#     plt.plot(grp['modulus'].apply(np.math.log2), grp['t_total'], label="Time w/ {0:0d} segment(s)".format(key+1), color=colors[key])   
-
-
-# formatter_time = matplotlib.ticker.FuncFormatter(lambda s, x: time.strftime('%M:%S', time.gmtime(s)))
-# ax1.yaxis.set_major_formatter(formatter_time)
-# ax1.set_xlabel('RSA Key Size')
-# ax1.set_ylabel('Running time (minutes:seconds)')
-# plt.xticks(np.arange(8,16), 1 << np.arange(8,16))
-# plt.legend(loc='best')
-# plt.savefig('overhead.pdf')
-# plt.show()
-
+fig.set_size_inches(10, 5)
+# fig.savefig('test2png.png', dpi=100)
+# plt.figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
+plt.savefig('modulus.pdf')
+plt.show()
 
 #%%
 
-overheadopt_data=pd.read_csv('/Users/Adrian/Desktop/graph_overhead_opt.csv', sep=',')
-overheadopt_data.values
+overhead_data=pd.read_csv('graph_overhead.csv', sep=',')
+overhead_data.values
 
 
 
@@ -95,33 +80,127 @@ colors = {
     2: 'r',
     3: 'c',
     4: 'm',
-    5: 'y'
+    5: 'y',
+    6: 'k'
+}
+#     0: 'b',
+#     1: 'g',
+#     2: 'r',
+#     3: 'c',
+#     4: 'm',
+# }
+
+for key, grp in overhead_data.groupby(['segments']):
+    if key == 0:      
+        plt.plot(grp['modulus'].apply(np.math.log2), grp['t_squaring'], label='Time squaring', linestyle='dashed', color=colors[4])
+    plt.plot(grp['modulus'].apply(np.math.log2), grp['t_total'], label="Time w/ {0:0d} segment(s)".format(key+1), color=colors[key])   
+
+
+formatter_time = matplotlib.ticker.FuncFormatter(lambda s, x: time.strftime('%H:%M:%S', time.gmtime(s)))
+ax1.yaxis.set_major_formatter(formatter_time)
+ax1.set_xlabel('RSA Key Size')
+ax1.set_ylabel('Running time (hours:minutes:seconds)')
+plt.xticks(np.arange(8,16), 1 << np.arange(8,16))
+plt.legend(loc='best')
+plt.savefig('overhead.pdf')
+plt.show()
+
+
+#%%
+
+overheadopt_data=pd.read_csv('graph_overhead_opt.csv', sep=',')
+overheadopt_data.values
+
+
+np.sum(overheadopt_data['t_total'])
+fig, ax1 = plt.subplots()
+
+colors = {
+    0: 'b',
+    1: 'g',
+    2: 'r',
+    3: 'c',
+    4: 'm',
+    5: 'y',
+    6: 'k'
 }
 
 for key, grp in overheadopt_data.groupby(['segments']):
     if key == 0:      
+        ax1.plot(grp['modulus'].apply(np.math.log2), grp['t_squaring'], label='Time squaring', linestyle='dashed', color=colors[0])
+    ax1.plot(grp['modulus'].apply(np.math.log2), grp['t_total'], label="Time w/ {} segment(s)".format(key+1), color=colors[key+1])   
+
+
+formatter_time = matplotlib.ticker.FuncFormatter(lambda s, x: time.strftime('%H:%M:%S', time.gmtime(s)))
+ax1.yaxis.set_major_formatter(formatter_time)
+ax1.set_xlabel('RSA Key Size')
+ax1.set_ylabel('Running time (hours:minutes:seconds)')
+plt.xticks(np.arange(8,16), 1 << np.arange(8,16))
+plt.legend(loc='best')
+
+# ax2 = ax1.twinx() 
+# ax2.set_ylabel(r'$\omega$', color='b')  # we already handled the x-label with ax1
+# for key, grp in overheadopt_data.groupby(['segments']):
+#     if key == 1:      
+#         ax2.plot(grp['modulus'].apply(np.math.log2), grp['overhead'], label='Time squaring', linestyle='dotted', color='b')
+
+# ax2.tick_params(axis='y', labelcolor='b', )
+
+
+
+plt.savefig('overheadopt.pdf')
+plt.show()
+
+
+#%%
+
+
+overheadopt_data=pd.read_csv('graph_overhead_opt.csv', sep=',')
+overhead_data=pd.read_csv('graph_overhead.csv', sep=',')
+
+plt.close('all')
+fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+
+for key, grp in overhead_data.groupby(['segments']):
+    if key == 0:      
         ax1.plot(grp['modulus'].apply(np.math.log2), grp['t_squaring'], label='Time squaring', linestyle='dashed', color=colors[4])
-    ax1.plot(grp['modulus'].apply(np.math.log2), grp['t_total'], label="Time w/ {} segment(s)".format(key+1), color=colors[key])   
+    ax1.plot(grp['modulus'].apply(np.math.log2), grp['t_total'], label="Time w/ {0:0d} segment(s)".format(key+1), color=colors[key])   
+
+
+ax1.legend(loc=2)
+
+
+for key, grp in overheadopt_data.groupby(['segments']):
+    if key == 0:      
+        ax2.plot(grp['modulus'].apply(np.math.log2), grp['t_squaring'], label='Time squaring', linestyle='dashed', color=colors[0])
+    ax2.plot(grp['modulus'].apply(np.math.log2), grp['t_total'], label="Time w/ {} segment(s)".format(key+1), color=colors[key+1])   
+
+# ax3 = ax2.twinx() 
+# ax3.set_ylabel(r'$\omega$', color='b')  # we already handled the x-label with ax1
+# for key, grp in overheadopt_data.groupby(['segments']):
+#     if key == 1:      
+#         ax3.plot(grp['modulus'].apply(np.math.log2), grp['overhead'], label='Time squaring', linestyle='dotted', color='b')
 
 
 formatter_time = matplotlib.ticker.FuncFormatter(lambda s, x: time.strftime('%M:%S', time.gmtime(s)))
 ax1.yaxis.set_major_formatter(formatter_time)
+ax2.yaxis.set_major_formatter(formatter_time)
 ax1.set_xlabel('RSA Key Size')
-ax1.set_ylabel('Running time (minutes:seconds)')
-plt.xticks(np.arange(8,16), 1 << np.arange(8,16))
-plt.legend(loc='best')
+ax2.set_xlabel('RSA Key Size')
+ax1.set_ylabel('Running time (hours:minutes:seconds)')
+plt.tight_layout()
 
-ax2 = ax1.twinx() 
-ax2.set_ylabel(r'$\omega$', color='y')  # we already handled the x-label with ax1
-for key, grp in overheadopt_data.groupby(['segments']):
-    if key == 1:      
-        ax2.plot(grp['modulus'].apply(np.math.log2), grp['overhead'], label='Time squaring', color=colors[5])
+ax2.tick_params(axis='y', labelcolor='b', )
+ax2.legend(loc=2)
 
-ax2.tick_params(axis='y', labelcolor='y')
+ax1.set_xticks(np.arange(8,16),)
+ax1.set_xticklabels(1 << np.arange(8,16))
+ax2.set_xticks(np.arange(8,16),)
+ax2.set_xticklabels(1 << np.arange(8,16))
 
+fig.set_size_inches(10, 5)
 
-
-plt.savefig('overhead.pdf')
+plt.savefig('overhead2.pdf')
 plt.show()
 
 
